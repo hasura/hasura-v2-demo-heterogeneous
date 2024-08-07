baebf8527be58cd517451ac4d55d110ca61d6b9e
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-08-07 09:42:11 -0700

    Adding metadata DDL

---
+++ initdb.d-metadata/03_chinook_database.sql
@@ -0,0 +1,11 @@
+-- -*- sql-product: postgres; -*-
+
+create database chinook;
+
+create database metadata_1;
+
+create database metadata_2;
+
+create database metadata_3;
+
+create database metadata_4;
